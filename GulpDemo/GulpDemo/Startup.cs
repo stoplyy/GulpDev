6727578c1bd4b8dc1cc0b434c2909886382a78e6
stop_lyy <1024717602@qq.com>
2016-07-21 17:23:48 +0800

    初始化

---
+++ GulpDemo/GulpDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GulpDemo.Startup))]
+namespace GulpDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
